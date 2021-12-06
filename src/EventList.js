7059a3f7b0e61b07ba0792f2b6f4ffa453fd62df
adexbam <32496428+adexbam@users.noreply.github.com>
2021-12-06 16:33:09 +0100

    create test folder and test for list of events

---
+++ src/EventList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Event from './Event';
+
+export default class EventList extends Component {
+    render() {
+        const {events} = this.props;
+        return (
+            <ul className="EventList">
+                {events.map(event => <li>
+                    <Event event={event} />
+                </li>)}
+            </ul>
+        )
+    }
+}
