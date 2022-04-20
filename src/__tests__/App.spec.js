0f69b93954d84d86ad182bbdaddd4e116a9669c8
adexbam <32496428+adexbam@users.noreply.github.com>
2022-04-20 15:03:22 +0200

    add unit test that renders citySearch component

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -2,11 +2,16 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "../App";
 import EventList from "../EventList";
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
     test('should render list of events', () => {
         const AppWrapper = shallow(<App />);
         expect(AppWrapper.find(EventList)).toHaveLength(1);
+    });
+    test('should render CitySearch', () => {
+        const AppWrapper = shallow(<App />);
+        expect(AppWrapper.find(CitySearch)).toHaveLength(1);
     });
 })
 
