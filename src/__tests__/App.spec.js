37e64414ea1fc62d946ade04caeefe57a46ac556
adexbam <32496428+adexbam@users.noreply.github.com>
2022-04-20 15:09:26 +0200

    Refactor code using the DRY clean code principle

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -5,12 +5,14 @@ import EventList from "../EventList";
 import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
+    let AppWrapper;
+    beforeAll(() => {
+        AppWrapper = shallow(<App />)
+    })
     test('should render list of events', () => {
-        const AppWrapper = shallow(<App />);
         expect(AppWrapper.find(EventList)).toHaveLength(1);
     });
     test('should render CitySearch', () => {
-        const AppWrapper = shallow(<App />);
         expect(AppWrapper.find(CitySearch)).toHaveLength(1);
     });
 })
